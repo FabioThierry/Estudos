54b3416be4270fa0fc47e6c05d60e1e8bca87041
Fábio Thierry <fabiodomingues.08@gmail.com>
2024-03-17 01:33:24 -0300

    Criação de testes com express

---
+++ javascript/exercicios/mvc-js/routes.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const route = express.Router();
+const homeController = require("./src/controllers/homeController");
+const contatoController = require("./src/controllers/contatoController");
+
+// Routes from home
+route.get("/", homeController.paginaInicial);
+route.get("/", homeController.trataPost);
+
+// Rotas de contato
+route.get("/contato", contatoController.paginaInicial);
+
+module.exports = route;
+
+// const express = require("express");
+// const route = express.Router();
+// const homeController = require("./src/controllers/homeController");
+// const contatoController = require("./src/controllers/contatoController");
+
+// // Rotas da home
+// route.get("/", homeController.paginaInicial);
+// route.post("/", homeController.trataPost);
+
+// // Rotas de contato
+// route.get("/contato", contatoController.paginaInicial);
+
+// module.exports = route;
