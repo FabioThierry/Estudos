63cfad26c2ea94b02aa812254ee05f691399cc79
FabioThierry <fabiodomingues.08@gmail.com>
2024-07-09 21:58:39 -0300

    Boilerplate do MVC

--- javascript/exercicios/mvc-js/routes.js
+++ javascript/exercicios/mvc-js/routes.js
@@ -5,23 +5,9 @@ const contatoController = require("./src/controllers/contatoController");
 
 // Routes from home
 route.get("/", homeController.paginaInicial);
-route.get("/", homeController.trataPost);
+route.post("/", homeController.trataPost);
 
 // Rotas de contato
 route.get("/contato", contatoController.paginaInicial);
 
 module.exports = route;
-
-// const express = require("express");
-// const route = express.Router();
-// const homeController = require("./src/controllers/homeController");
-// const contatoController = require("./src/controllers/contatoController");
-
-// // Rotas da home
-// route.get("/", homeController.paginaInicial);
-// route.post("/", homeController.trataPost);
-
-// // Rotas de contato
-// route.get("/contato", contatoController.paginaInicial);
-
-// module.exports = route;
--- javascript/exercicios/mvc-js/server.js
+++ javascript/exercicios/mvc-js/server.js
@@ -1,18 +1,59 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+mongoose
+  .connect(process.env.CONNECTION_STRING)
+  .then(() => {
+    console.log("Conectado ao MongoDB");
+    app.emit("pronto");
+  })
+  .catch((e) => console.log(e));
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+const flash = require("connect-flash");
 const routes = require("./routes");
 const path = require("path");
-const port = 3000;
+const helmet = require("helmet");
+const csrf = require("csurf");
+const {
+  middlewareGlobal,
+  csrfMiddleware,
+  checkCsrfError,
+} = require("./src/middlewares/middleware");
 
+app.use(helmet());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static(path.resolve(__dirname, "public")));
+
+const sessionOptions = session({
+  secret: "seusecreto",
+  store: MongoStore.create({ mongoUrl: process.env.CONNECTION_STRING }),
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+    httpOnly: true,
+  },
+});
+app.use(sessionOptions);
+app.use(flash());
 
 app.set("views", path.resolve(__dirname, "src", "views"));
 app.set("view engine", "ejs");
 
+app.use(csrf());
+// Meus Middlewares
+app.use(middlewareGlobal);
+app.use(csrfMiddleware);
+app.use(checkCsrfError);
 app.use(routes);
 
-app.listen(port, () => {
-  console.log("Acessar o servidor: http://localhost:" + port);
-  console.log(`rodando na porta ${port}`);
+const port = 3000;
+app.on("pronto", () => {
+  app.listen(port, () => {
+    console.log("Acessar o servidor: http://localhost:" + port);
+    console.log(`rodando na porta ${port}`);
+  });
 });
