265f77c469e1410deb1f619d0bfa8255665af495
FabioThierry <fabiodomingues.08@gmail.com>
2024-07-24 01:48:02 -0300

    correção Link react

--- frontend/react/projetos/escola-react/src/pages/Login/index.jsx
+++ frontend/react/projetos/escola-react/src/pages/Login/index.jsx
@@ -3,17 +3,16 @@ import { Title } from "./styled";
 import { Container } from "../../styles/GlobalStyles";
 import React from "react";
 
-// import axios from "../../services/axios";
+import axios from "../../services/axios";
 export default function Login() {
   React.useEffect(() => {
-    console.log("oi");
-    // async function getData() {
-    //   const response = await axios.get("/alunos");
-    //   const { data } = response;
-    //   console.log(data);
-    // }
+    async function getData() {
+      const response = await axios.get("/alunos");
+      const { data } = response;
+      console.log(data);
+    }
 
-    // getData();
+    getData();
   }, []);
   return (
     <>
