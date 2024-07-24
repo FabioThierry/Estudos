df8e9ca30c7d23ba6fae542d171dddb76c0af3db
FabioThierry <fabiodomingues.08@gmail.com>
2024-07-24 12:35:58 -0300

    Antes de iniciar o Redux

--- frontend/react/projetos/escola-react/src/pages/Login/index.jsx
+++ frontend/react/projetos/escola-react/src/pages/Login/index.jsx
@@ -1,19 +1,8 @@
 import { Title } from "./styled";
 
 import { Container } from "../../styles/GlobalStyles";
-import React from "react";
 
-import axios from "../../services/axios";
 export default function Login() {
-  React.useEffect(() => {
-    async function getData() {
-      const response = await axios.get("/alunos");
-      const { data } = response;
-      console.log(data);
-    }
-
-    getData();
-  }, []);
   return (
     <>
       <Container>
